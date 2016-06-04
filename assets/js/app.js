576d04cfa515fe416dd855c1369b337040c7fea4
Joey Reyes <jreyes88@gmail.com>
2016-06-04 11:38:50 -0500

    the form is working and plugging info into firebase

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,30 +1,22 @@
 var currentEmployeesTable = new Firebase("https://employeedatamanager.firebaseio.com/");
 
-// function displayTime() {
-//    var time = moment().format('hh:mm:ss a');
-//    $('#currentTime').html("Current Time Is:  " + time);
-//    //$('#currentTime').append(time);
-//    setTimeout(displayTime, 1000);
-// };
-
-// currentEmployeesTable.on("value", function(snapshot) {
-
-// })
-
 $(".submitButton").on("click", function() {
 	var nameOfEmployee = $('#calcEmployeeName').val().trim();
 	var roleOfEmployee = $('#calcRole').val().trim();
 	var employeeStartDate = $('#calcStartDate').val().trim();
 	var employeePayRate = $('#calcRate').val().trim();
 
-	console.log(nameOfEmployee);
-	console.log(roleOfEmployee);
-	console.log(employeeStartDate);
-	console.log(employeePayRate);
-})
+	currentEmployeesTable.push({
+		name:nameOfEmployee,
+		role:roleOfEmployee,
+		start:employeeStartDate,
+		rate:employeePayRate
+	});
 
-currentEmployeesTable.on("value", function(snapshot) {
-	
+	$("form")[0].reset();
+	return false;
+});
 
-	
+currentEmployeesTable.on("child_added", function(childSnapShot) {
+	console.log(childSnapShot.val().name);
 })
